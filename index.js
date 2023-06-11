99d1cd1ca4d7796d3ecdcf060d99bb4d6d353b83
Pronin Nikita <proninnikitaalex@gmail.com>
2023-06-11 12:50:00 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        apikey: 'd9835cc5',
+        s: 'avengers'
+      }
+    });
+  
+    console.log(response.data);
+  };
+  
+  fetchData();
+  
